884883b73971b307bd43b07531218a92e898689a
antoniogiachin <giachinantonio@outlook.com>
2022-02-21 15:38:30 +0100

    header stilizzato

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -1,5 +1,57 @@
 <template>
-    <div class="container"></div>
+    <header>
+
+        <div class="container">
+
+            <!-- header left -->
+            <div class="header-left">
+                <div class="logo">
+                    <img src="../assets/img/dc-logo.png" alt="dc-logo-header">
+                </div>
+            </div>
+
+            <!-- header right -->
+            <div class="header-right">
+
+                <ul>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="active">home</a>
+                    </li>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+                </ul>
+
+            </div>
+
+
+        </div>
+
+    </header>
 </template>
 
 <script>
@@ -13,5 +65,69 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    @import "../assets/style/variables.scss"
+    @import "../assets/style/variables.scss";
+
+    header{
+        height: 80px;
+        @include my_flex_row;
+        .container{
+            height: 100%;
+            @include my_flex_row;
+            justify-content: space-between;
+
+            .header-left{
+                width: 30%;
+                @include my_flex_row;
+                justify-content: center;
+
+                .logo{
+                    @include my_flex_row;
+                    justify-content: center;
+                    width: 60px;
+                    height: 60px;
+
+                    img{
+                        width: 100%;
+                        height: 100%;
+                        object-fit: contain;
+                        
+                    }
+                }
+            }
+
+            .header-right{
+                text-transform: uppercase;
+                height: 100%;
+                width: 50%;
+
+                ul{
+                    list-style-type: none;
+                    height: 100%;
+                    @include my_flex_row;
+                    justify-content: space-around;
+
+                    li{
+                        margin: 0px 10px;
+
+                        a{
+                            color: $colorDark;
+                            height: 80px;
+                            display: inline-block;
+                            @include my_flex_row;
+                            text-decoration: none;
+                            font-size: 0.8em;
+                            border-bottom: 5px solid transparent;
+
+                            &.active, &:hover{
+                                border-bottom: 5px solid $colorBlue;
+                                color:$colorBlue
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
 </style>
