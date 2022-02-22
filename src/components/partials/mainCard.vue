ac6f16ad6fe94a9940421677190e4d85f619f8af
antoniogiachin <giachinantonio@outlook.com>
2022-02-22 16:20:56 +0100

    inserito contenuto main dinamico

---
+++ src/components/partials/mainCard.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div class="main-card" >
+
+        <div class="card-img">
+          <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="main-card-img">
+        </div>
+        
+        <div class="card-text">
+          <span>action comics</span>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'mainCard'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "../assets/style/variables.scss";
+
+</style>
