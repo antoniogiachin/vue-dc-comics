bc0348d0563175052bd3d8306375250fb7e3a658
antoniogiachin <giachinantonio@outlook.com>
2022-02-22 15:38:29 +0100

    inserito jumbotron e div current series

--- src/components/myMain.vue
+++ src/components/myMain.vue
@@ -1,6 +1,15 @@
 <template>
   <main>
+    
+    <!-- jumbotron -->
+    <section id="jumbotron">
+
+    </section>
+
     <div class="container">
+
+      <div class="current-series"><span>current series</span></div>
+
       <p>--> Content goes Here  </p>
     </div>
   </main>
@@ -24,10 +33,30 @@ export default {
       color : $colorWhite;
       font-weight: bold;
 
+      #jumbotron{
+        min-height: 350px;
+        width: 100%;
+        background-image: url(../assets/img/jumbotron.jpg);
+        background-size: cover;
+      }
+
       .container{
         height: 100px;
         @include my_flex_row;
+        position: relative;
 
+        .current-series{
+          display: inline-block;
+          position: absolute;
+          top: -20px;
+          background-color: $colorBlue;
+          text-transform: uppercase;
+          color:$colorWhite;
+          padding: 5px 20px;
+          font-size: 1.2em;
+          font-weight: bold;
+        }
       }
+
     }
 </style>
